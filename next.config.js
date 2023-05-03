3320b9aa79ae23fdfdf715f2b1258bdfed1fb1f3
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-03 15:26:13 +0700

    revert next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const path = require("path");
-const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -14,44 +13,35 @@ const nextConfig = {
   },
 };
 
-async function buildPublicRuntimeConfig() {
-  const [routesRes, settingRes, productAttributesRes] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}page/main-page`),
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}setting/fetch-all`),
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}product-attribute/fetch-all`),
-  ]);
-
-  const [routes, settings, productAttributes] = await Promise.all([
-    routesRes.json(),
-    settingRes.json(),
-    productAttributesRes.json(),
-  ]);
-
-  return {
-    routes: routes.data,
-    settings: settings.data,
-    productAttributes: productAttributes.data,
-  };
-}
-
-const sentryWebpackPluginOptions = {
-  org: "phongtran-9e",
-  project: "halo",
-  silent: true, // Suppresses all logs
-};
-
-module.exports = async () => {
+module.exports = async (phase, { defaultConfig }) => {
   try {
-    const publicRuntimeConfigRes = await buildPublicRuntimeConfig();
+    const [routesRes, settingRes, productAttributesRes] = await Promise.all([
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}page/main-page`),
+      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}setting/fetch-all`),
+      fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API}product-attribute/fetch-all`
+      ),
+    ]);
 
-    nextConfig.publicRuntimeConfig = await publicRuntimeConfigRes;
-    nextConfig.sentry = {
-      hideSourceMaps: true,
-      enabled: process.env.NODE_ENV === "production",
+    const [routes, settings, productAttributes] = await Promise.all([
+      routesRes.json(),
+      settingRes.json(),
+      productAttributesRes.json(),
+    ]);
+
+    return {
+      defaultConfig,
+      publicRuntimeConfig: {
+        routes: routes.data,
+        settings: settings.data,
+        productAttributes: productAttributes.data,
+      },
+      ...nextConfig,
     };
-
-    return withSentryConfig(nextConfig, sentryWebpackPluginOptions);
   } catch (e) {
-    return withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+    return {
+      defaultConfig,
+      ...nextConfig,
+    };
   }
 };
