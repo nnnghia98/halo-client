e10e6c37193151c9a6e993ef30e67c893fd382a0
phongtran <phong@sleek.com>
2023-05-03 14:03:24 +0700

    add sentry

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@
 const path = require("path");
 const { withSentryConfig } = require("@sentry/nextjs");
 
+async function buildNextConfig(config) {
+  const [routesRes, settingRes, productAttributesRes] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}page/main-page`),
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}setting/fetch-all`),
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}product-attribute/fetch-all`),
+  ]);
+
+  const [routes, settings, productAttributes] = await Promise.all([routesRes.json(), settingRes.json(), productAttributesRes.json()]);
+  config.publicRuntimeConfig = {
+    routes: routes.data,
+      settings: settings.data,
+      productAttributes: productAttributes.data,
+  };
+}
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
@@ -18,36 +33,12 @@ const nextConfig = {
   }
 };
 
+buildNextConfig(nextConfig);
+
 const sentryWebpackPluginOptions = {
   org: "phongtran-9e",
   project: "halo",
   silent: true, // Suppresses all logs
 };
 
-module.exports = withSentryConfig(async (phase, { defaultConfig }) => {
-  try {
-    const [routesRes, settingRes, productAttributesRes] = await Promise.all([
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}page/main-page`),
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}setting/fetch-all`),
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}product-attribute/fetch-all`),
-    ]);
-
-    const [routes, settings, productAttributes] = await Promise.all([routesRes.json(), settingRes.json(), productAttributesRes.json()]);
-
-    return {
-      sentryWebpackPluginOptions,
-      defaultConfig,
-      publicRuntimeConfig: {
-        routes: routes.data,
-        settings: settings.data,
-        productAttributes: productAttributes.data,
-      },
-      ...nextConfig,
-    };
-  } catch (e) {
-    return {
-      defaultConfig,
-      ...nextConfig,
-    };
-  }
-});
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
