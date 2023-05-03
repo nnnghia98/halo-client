86b4a106d2f28250ac9b588084302a5ea4036fd4
phongtran <phong@sleek.com>
2023-05-03 18:52:00 +0700

    add sentry

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 async function buildNextConfig(config) {
   const [routesRes, settingRes, productAttributesRes] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}page/main-page`),
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}setting/fetch-all`),
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}product-attribute/fetch-all`),
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/page/main-page`),
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/setting/fetch-all`),
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/product-attribute/fetch-all`),
   ]);
 
   const [routes, settings, productAttributes] = await Promise.all([routesRes.json(), settingRes.json(), productAttributesRes.json()]);
