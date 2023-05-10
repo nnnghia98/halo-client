efc54620ce9e4d53779d47121e8661605ea0537a
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-10 17:23:49 +0700

    change product detail carousel image size

--- modules/ProductDetailImages/index.js
+++ modules/ProductDetailImages/index.js
@@ -22,7 +22,7 @@ const Images = ({ thumbnail, sliders = [] }) => {
           src={currentImage ?? defaultProductImg}
           alt=""
           width="600"
-          height="600"
+          height="450"
         />
       </div>
       {sliders.length > 0 && (
@@ -33,8 +33,8 @@ const Images = ({ thumbnail, sliders = [] }) => {
                 <Image
                   src={slider.thumbnail}
                   style={{ cursor: "pointer" }}
-                  width="200"
-                  height="200"
+                  width="250"
+                  height="190"
                   alt={slider.name}
                   onClick={() => handleCarouselItemClick(slider.thumbnail)}
                 />
