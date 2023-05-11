2b9851c776929275164d41022ef7cd813477a033
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-11 22:58:31 +0700

    fix breadcrumb

--- components/Breadcrumb/index.js
+++ components/Breadcrumb/index.js
@@ -1,26 +1,44 @@
 import React from "react";
-import styles from "./Breadcrumb.module.scss";
-import {PRODUCT_PAGE} from "utils/constants";
 import Link from "next/link";
+import isEqual from "lodash/isEqual";
 import cloneDeep from "lodash/cloneDeep";
 
-const Breadcrumb = ({paths}) => {
+import { PRODUCT_PAGE } from "utils/constants";
+
+import styles from "./Breadcrumb.module.scss";
+
+const Breadcrumb = ({ paths }) => {
   const breadcrumbs = cloneDeep(paths);
+
   breadcrumbs.unshift({
     title: PRODUCT_PAGE.title,
-    slug: "/"
+    slug: "/",
   });
+
   return (
     <div className={styles.breadcrumb}>
-      {breadcrumbs && breadcrumbs.map((path, index) => (
-        <span className={styles.breadcrumb__path} key={path.title}
-              style={{color: index < breadcrumbs.length - 1 ? "#999999" : ""}}>
-        {path.slug ? (<Link href={`/${PRODUCT_PAGE.slug}/${path.slug}`}>
-          <a> {path.title} / </a>
-        </Link>) : path.title}
-      </span>))}
+      {breadcrumbs &&
+        breadcrumbs.map((path, index) => (
+          <span
+            className={styles.breadcrumb__path}
+            key={path.title}
+            style={{ color: index < breadcrumbs.length - 1 ? "#999999" : "" }}
+          >
+            {path.slug ? (
+              <Link
+                href={`/${PRODUCT_PAGE.slug}${
+                  isEqual(path.slug, "/") ? "/" : `/${path.slug}`
+                }`}
+              >
+                <a> {path.title} / </a>
+              </Link>
+            ) : (
+              path.title
+            )}
+          </span>
+        ))}
     </div>
-  )
+  );
 };
 
 export default Breadcrumb;
--- modules/ProductDetailImages/index.js
+++ modules/ProductDetailImages/index.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { Item } from "react-grid-carousel";
+import isEmpty from "lodash/isEmpty";
 
 import { HorizontalCarousel } from "components";
 
@@ -11,18 +12,18 @@ const Images = ({ thumbnail, sliders = [] }) => {
   const [currentImage, setCurrentImage] = useState(
     thumbnail.path ?? defaultProductImg
   );
-  const [currentSlider, setCurrentSlider] = useState(sliders);
+
+  const joinedSlider = [
+    {
+      thumbnail: thumbnail.path ?? defaultProductImg,
+      name: thumbnail.name,
+    },
+    ...sliders,
+  ];
 
   const handleCarouselItemClick = (thumbnailUrl) => {
     setCurrentImage(thumbnailUrl);
   };
-
-  useEffect(() => {
-    setCurrentSlider([
-      { thumbnail: thumbnail.path, name: thumbnail.name },
-      ...sliders,
-    ]);
-  }, []);
 
   return (
     <div className={styles.images}>
@@ -30,20 +31,22 @@ const Images = ({ thumbnail, sliders = [] }) => {
         <Image priority src={currentImage} alt="" width="600" height="450" />
       </div>
       <div className={styles.images__slideshow}>
-        <HorizontalCarousel cols={4} rows={1} loop autoplay={3000} gap={20}>
-          {currentSlider.map((slider) => (
-            <Item key={slider.name} className={styles.images__smImage}>
-              <Image
-                src={slider.thumbnail}
-                style={{ cursor: "pointer" }}
-                width="250"
-                height="190"
-                alt={slider.name}
-                onClick={() => handleCarouselItemClick(slider.thumbnail)}
-              />
-            </Item>
-          ))}
-        </HorizontalCarousel>
+        {!isEmpty(thumbnail.path) && (
+          <HorizontalCarousel cols={4} rows={1} loop autoplay={3000} gap={20}>
+            {joinedSlider.map((slider) => (
+              <Item key={slider.name} className={styles.images__smImage}>
+                <Image
+                  src={slider.thumbnail}
+                  style={{ cursor: "pointer" }}
+                  width="250"
+                  height="190"
+                  alt={slider.name}
+                  onClick={() => handleCarouselItemClick(slider.thumbnail)}
+                />
+              </Item>
+            ))}
+          </HorizontalCarousel>
+        )}
       </div>
     </div>
   );
