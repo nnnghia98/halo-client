b98028d4bcd439cea141e66a103727aa9aad889a
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-11 22:32:37 +0700

    use state for sliders

--- modules/ProductDetailImages/index.js
+++ modules/ProductDetailImages/index.js
@@ -8,45 +8,43 @@ import defaultProductImg from "assets/img/default_product.png";
 import styles from "./ProductDetailImages.module.scss";
 
 const Images = ({ thumbnail, sliders = [] }) => {
-  const [currentImage, setCurrentImage] = useState(thumbnail.path || null);
+  const [currentImage, setCurrentImage] = useState(
+    thumbnail.path ?? defaultProductImg
+  );
+  const [currentSlider, setCurrentSlider] = useState(sliders);
 
   const handleCarouselItemClick = (thumbnailUrl) => {
     setCurrentImage(thumbnailUrl);
   };
 
   useEffect(() => {
-    sliders.push({ thumbnail: thumbnail.path, name: thumbnail.name });
+    setCurrentSlider([
+      { thumbnail: thumbnail.path, name: thumbnail.name },
+      ...sliders,
+    ]);
   }, []);
 
   return (
     <div className={styles.images}>
       <div className={styles.images__mainImage}>
-        <Image
-          priority
-          src={currentImage ?? defaultProductImg}
-          alt=""
-          width="600"
-          height="450"
-        />
+        <Image priority src={currentImage} alt="" width="600" height="450" />
       </div>
-      {sliders.length > 0 && (
-        <div className={styles.images__slideshow}>
-          <HorizontalCarousel cols={4} rows={1} loop autoplay={3000} gap={20}>
-            {sliders.map((slider) => (
-              <Item key={slider.name} className={styles.images__smImage}>
-                <Image
-                  src={slider.thumbnail}
-                  style={{ cursor: "pointer" }}
-                  width="250"
-                  height="190"
-                  alt={slider.name}
-                  onClick={() => handleCarouselItemClick(slider.thumbnail)}
-                />
-              </Item>
-            ))}
-          </HorizontalCarousel>
-        </div>
-      )}
+      <div className={styles.images__slideshow}>
+        <HorizontalCarousel cols={4} rows={1} loop autoplay={3000} gap={20}>
+          {currentSlider.map((slider) => (
+            <Item key={slider.name} className={styles.images__smImage}>
+              <Image
+                src={slider.thumbnail}
+                style={{ cursor: "pointer" }}
+                width="250"
+                height="190"
+                alt={slider.name}
+                onClick={() => handleCarouselItemClick(slider.thumbnail)}
+              />
+            </Item>
+          ))}
+        </HorizontalCarousel>
+      </div>
     </div>
   );
 };
