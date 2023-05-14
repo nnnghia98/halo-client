f208945ccb3b3ba84a19f2c37004df39c860c9bf
Nghia Nguyen <nnnghia98@gmail.com>
2023-05-14 23:20:26 +0700

    home projects

--- modules/Projects/index.js
+++ modules/Projects/index.js
@@ -1,42 +1,51 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import isEmpty from "lodash/isEmpty";
 
 import { Breadcrumb } from "components";
 import WebContent from "../WebContent";
 import { ProductRecommend, Subscribe, MobileContent } from "modules";
 
 import styles from "./Projects.module.scss";
-import isEmpty from "lodash/isEmpty";
-import { useRouter } from "next/router";
-import { PROJECT_PAGE } from "utils/constants";
+import { PROJECT_PAGE, PRODUCT_PAGE } from "utils/constants";
 
 const Projects = ({ postsProject = [] }) => {
   const router = useRouter();
+
   const handleNavigate = (post) => {
-    router.push(`/${PROJECT_PAGE.slug}/${post.slug}`)
-  }
+    router.push(`/${PROJECT_PAGE.slug}/${post.slug}`);
+  };
+
   return (
     <div className={styles.projects}>
       <div className={styles.projects__container}>
         <h3>DỰ ÁN THƯƠNG HIỆU</h3>
-        {
-          !isEmpty(postsProject) && postsProject.map(post => (
-            <div key={post.title} className={styles.projects__itemWrap} onClick={() => handleNavigate(post)}>
-              <div className={styles.projects__itemName}>{post.title}</div>
-            </div>
-          ))
-        }
-        {/* <div className={styles.projects__list}>
-          <div className={styles.projects__itemWrap}>
-            <div className={styles.projects__itemName}>PRIVATE VILLA</div>
-            <div className={styles.projects__itemLocation}>
-              Ecopark Green City - Hà Nội
-            </div>
+        {!isEmpty(postsProject) && (
+          <div className={styles.projects__list}>
+            {postsProject.map((post) => (
+              <Link key={post.slug} href={`/${PROJECT_PAGE.slug}/${post.slug}`}>
+                <div className={styles.projects__itemWrap}>
+                  {/* <Image
+                  className={styles.productsList__itemImg}
+                  src={product.thumbnail ?? defaultProductImg}
+                  alt={product.thumbnail_name ?? "Product image"}
+                  layout="fill"
+                /> */}
+                  <div className={styles.projects__itemName}>{post.title}</div>
+                  <div className={styles.projects__itemLocation}>
+                    {post.description}
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
-        </div> */}
+        )}
         <div className={styles.projects__more}>XEM THÊM</div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Projects;
