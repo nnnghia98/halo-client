5f41c48b0c560cb8c350ca3749d85fc2fb3655ae
phongtran <phong@sleek.com>
2023-05-14 10:24:58 +0700

    update project index

--- modules/ProjectGallery/index.js
+++ modules/ProjectGallery/index.js
@@ -1,88 +1,34 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
 import project1 from "assets/img/project1.jpg";
-
 import styles from "./ProjectGallery.module.scss";
+import isEmpty from "lodash/isEmpty";
+import { PROJECT_PAGE } from "utils/constants";
 
-const ProjectGallery = () => (
-  <div className={styles.productGallery}>
-    <div className={styles.productGallery__list}>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
-      <Link href="/bo-suu-tap/detail">
-        <div className={styles.productGallery__itemWrap}>
-          <Image src={project1} alt="project-1" priority />
-          <div className={styles.productGallery__itemName}>
-            <div>PRIVATE RESIDENCE</div>
-            <div>The Signature Midtown, Phú Mỹ Hưng</div>
-          </div>
-        </div>
-      </Link>
+const ProjectGallery = ({ postsProject = [] }) => {
+  return (
+    <div className={styles.productGallery}>
+      <div className={styles.productGallery__list}>
+        {
+          !isEmpty(postsProject) && postsProject.map(post => (
+            <Link key={post.title} href={`${PROJECT_PAGE.slug}/${post.slug}`}>
+              <div className={styles.productGallery__itemWrap}>
+                <Image src={post.thumbnail ?? project1} alt={post.thumbnail_name ?? "project-1"} 
+                  width={1000}
+                  height={750}
+                />
+                <div className={styles.productGallery__itemName}>
+                  <div>{post.title}</div>
+                  <div>{post.description}</div>
+                </div>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default ProjectGallery;
--- pages/cac-du-an/index.js
+++ pages/cac-du-an/index.js
@@ -1,10 +1,24 @@
+import HeadTitle from "components/HeadTitle";
 import { ProjectHeader, ProjectGallery } from "modules";
+import { getPostsByType } from "apis/post";
 
-const Projects = () => (
+const Projects = ({postsProject}) => (
   <>
+    <HeadTitle title="123" />
     <ProjectHeader />
-    <ProjectGallery />
+    <ProjectGallery postsProject={postsProject} />
   </>
 );
+
+export const getServerSideProps = async () => {
+  const [postsProject] = await Promise.all([
+    getPostsByType("project")
+  ]);
+  return {
+    props: {
+      postsProject: postsProject.data,
+    }
+  }
+}
 
 export default Projects;
